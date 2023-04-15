89a64288d4d6159e1f911bef2e231cf7d44c0367
Artful Info <artfulinfoco@gmail.com>
2023-04-15 11:23:00 +0100

    Script to cd into code project and activate venv

--- quickcode.sh
+++ quickcode.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 code
 xte 'keydown Shift_L' 'keydown Super_L' 'key Right' 'key m' 'keyup Shift_L' 'keyup Super_L' 
-gnome-terminal -- tmux
+gnome-terminal -- "tmux"
 sleep 0.1
 xte 'keydown Control_L' 'keydown Super_L' 'key Left' 'keyup Control_L' 'keydown Shift_L' 'key Left' 'keyup Shift_L' 'keyup Super_L'
 firefox

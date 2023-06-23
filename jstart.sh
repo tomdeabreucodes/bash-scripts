4c3e0fd4c06d330995fd5cca38412e0a134ed931
Artful Info <artfulinfoco@gmail.com>
2023-06-23 20:44:19 +0100

    Add quickstart for node apps

---
+++ jstart.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# When executing add source before calling the script
+# to cd in and activate venv in the current terminal.
+# Alternatively this can be added in the alias command .bashrc
+DIR=/home/artfvl/Documents/code/$1
+cd $DIR
+npm run dev
